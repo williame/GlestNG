8b01fd78c93bf92aa621efc8d4fef4bc6346b076
Will <anony@mous.me>
2011-03-02 16:46:27 +0100

    Single spline

--- prototyping/roads.py
+++ prototyping/roads.py
@@ -27,6 +27,55 @@ class Ground:
             if I is not None:
                 return I
                 
+def HermiteInterpolate(y0,y1,y2,y3,mu,tension,bias):
+    # from http://paulbourke.net/miscellaneous/interpolation/
+    tension = (1.-tension)/2
+    mu2 = mu * mu
+    mu3 = mu2 * mu
+    m0  = (y1-y0)*(1.+bias)*tension
+    m0 += (y2-y1)*(1.-bias)*tension
+    m1  = (y2-y1)*(1.+bias)*tension
+    m1 += (y3-y2)*(1.-bias)*tension
+    a0 =  2*mu3 - 3*mu2 + 1
+    a1 =    mu3 - 2*mu2 + mu
+    a2 =    mu3 -   mu2
+    a3 = -2*mu3 + 3*mu2
+    return(a0*y1+a1*m0+a2*m1+a3*y2)
+    
+def smooth(Y): # Y is a path of points
+    return Y
+
+def HermiteSpline(Y): # Y is a path of points
+    if len(Y) < 2: return
+    SEG = 10
+    tension, bias = 0.1, 0.
+    vertices = []
+    for y in xrange(len(Y)-1):
+        y0 = Y[y-1] if (y > 0) else Y[0]
+        y1 = Y[y]
+        y2 = Y[y+1]
+        y3 = Y[y+2] if (y < len(Y)-2) else Y[-1]
+        for mu in xrange(SEG+1):
+            mu *= 1./SEG
+            vertices.append((
+                HermiteInterpolate(y0[0],y1[0],y2[0],y3[0],mu,tension,bias),
+                HermiteInterpolate(y0[1],y1[1],y2[1],y3[1],mu,tension,bias),
+                HermiteInterpolate(y0[2],y1[2],y2[2],y3[2],mu,tension,bias)))
+    if len(vertices)<2:
+        return
+    print len(Y),"parts in path"
+    print "  ->",len(vertices),"vertices in path"
+    vertices = smooth(vertices)
+    print "  ->",len(vertices),"smoothed vertices in path"
+    glBegin(GL_LINE_STRIP)
+    for i,vertex in enumerate(vertices):
+        if i&1 == 1:
+            glColor(0,0,1,1)
+        else:
+            glColor(0,1,1,1)
+        glVertex(*vertex)
+    glEnd()
+
 def quad_line(p1,p2,width=0.05):
     x1, y1, z1 = p1
     x2, y2, z2 = p2
@@ -52,13 +101,6 @@ class RoadMaker(zpr.GLZPR):
         glScale(.8,.8,.8)        
         glDisable(GL_DEPTH_TEST)
         self.ground.draw()
-        # draw path
-        glColor(1,1,1,1)
-        glBegin(GL_QUADS)
-        for i in xrange(1,len(self.path)):
-            for vertex in quad_line(self.path[i-1],self.path[i]):
-                glVertex(*vertex)
-        glEnd()
         if self.end_point is not None:
             # draw a cross
             glColor(1,0,0,1)
@@ -77,6 +119,9 @@ class RoadMaker(zpr.GLZPR):
             glVertex(1,0,.2)
             glEnd()
             glPopMatrix()
+        glColor(0,0,1,1)
+        glLineWidth(4)
+        HermiteSpline(self.path)
         glEnable(GL_DEPTH_TEST)
             
     def _pick(self,x,y,dx,dy,event):
