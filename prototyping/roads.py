0072ce482968fc62c43de1c737f7ed743d19573d
Will <anony@mous.me>
2011-03-02 17:57:08 +0100

    Simple triangulation

--- prototyping/roads.py
+++ prototyping/roads.py
@@ -41,15 +41,12 @@ def HermiteInterpolate(y0,y1,y2,y3,mu,tension,bias):
     a2 =    mu3 -   mu2
     a3 = -2*mu3 + 3*mu2
     return(a0*y1+a1*m0+a2*m1+a3*y2)
-    
-def smooth(Y): # Y is a path of points
-    return Y
 
 def HermiteSpline(Y): # Y is a path of points
-    if len(Y) < 2: return
+    if len(Y) < 2: return ()
     SEG = 10
     tension, bias = 0.1, 0.
-    vertices = []
+    spline = []
     for y in xrange(len(Y)-1):
         y0 = Y[y-1] if (y > 0) else Y[0]
         y1 = Y[y]
@@ -57,24 +54,46 @@ def HermiteSpline(Y): # Y is a path of points
         y3 = Y[y+2] if (y < len(Y)-2) else Y[-1]
         for mu in xrange(SEG+1):
             mu *= 1./SEG
-            vertices.append((
+            spline.append((y,mu,(
                 HermiteInterpolate(y0[0],y1[0],y2[0],y3[0],mu,tension,bias),
                 HermiteInterpolate(y0[1],y1[1],y2[1],y3[1],mu,tension,bias),
-                HermiteInterpolate(y0[2],y1[2],y2[2],y3[2],mu,tension,bias)))
-    if len(vertices)<2:
-        return
-    print len(Y),"parts in path"
-    print "  ->",len(vertices),"vertices in path"
-    vertices = smooth(vertices)
-    print "  ->",len(vertices),"smoothed vertices in path"
-    glBegin(GL_LINE_STRIP)
-    for i,vertex in enumerate(vertices):
-        if i&1 == 1:
-            glColor(0,0,1,1)
+                HermiteInterpolate(y0[2],y1[2],y2[2],y3[2],mu,tension,bias))))
+    return spline
+    
+def triangles(pt,width):
+    halfwidth = width/2 
+    sqrwidth = width**2
+    def side(a,b,c):
+        _,_,(x1,y1,z1) = a
+        _,_,(x2,y2,z2) = b
+        _,_,(x3,y3,z3) = c
+        theta = math.atan2(z3 - z1,x3 - x1)
+        sin = halfwidth * math.sin(theta)
+        cos = halfwidth * math.cos(theta)
+        l = (x2+sin,y2,z2-cos)
+        r = (x2-sin,y2,z2+cos)
+        return (l,r)
+    tri = []
+    left,right = side(pt[0],pt[0],pt[1])
+    for i in xrange(len(pt)):
+        l,r = side(pt[i],pt[min(i+1,len(pt)-1)],pt[min(i+2,len(pt)-1)])
+        if sum((p-q)**2 for p,q in zip(l,left)) > sqrwidth:
+            tri.append((left,right,l))
+            left = l
+        if sum((p-q)**2 for p,q in zip(r,right)) > sqrwidth:
+            tri.append((right,left,r))
+            right = r
+    glColor(1,1,1)
+    glBegin(GL_TRIANGLES)
+    for i,t in enumerate(tri):
+        if i % 2 == 1:
+            glColor(0,.5,1)
         else:
-            glColor(0,1,1,1)
-        glVertex(*vertex)
+            glColor(.5,1,0)
+        for v in t:
+            glVertex(*v)
     glEnd()
+    return tri
 
 def quad_line(p1,p2,width=0.05):
     x1, y1, z1 = p1
@@ -95,6 +114,7 @@ class RoadMaker(zpr.GLZPR):
         self.ground = Ground()
         self.path = []
         self.end_point = None
+        self.info = False
     def draw(self,event):
         glClearColor(1,1,1,0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
@@ -119,9 +139,20 @@ class RoadMaker(zpr.GLZPR):
             glVertex(1,0,.2)
             glEnd()
             glPopMatrix()
-        glColor(0,0,1,1)
         glLineWidth(4)
-        HermiteSpline(self.path)
+        spline = HermiteSpline(self.path)
+        if len(spline) > 1:
+            tri = triangles(spline,0.05)
+            glColor(1,1,1)
+            glBegin(GL_LINE_STRIP)
+            for _,_,v in spline:
+                glVertex(*v)
+            glEnd()
+            if self.info:
+                self.info = False
+                print len(self.path),"points"
+                print "  ->",len(spline),"centre-line"
+                print "  ->",len(tri),"triangles"
         glEnable(GL_DEPTH_TEST)
             
     def _pick(self,x,y,dx,dy,event):
@@ -137,6 +168,7 @@ class RoadMaker(zpr.GLZPR):
         if point is not None:
             self.end_point = point
             self.path.append(self.end_point)
+            self.info = True
         return ((),())
     def pick(self,*args):
         pass
